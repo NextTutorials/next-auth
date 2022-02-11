92a3fe369b2af7aa3f7bd66ad9fbe37e14139bcb
Johan Melin <soldierheart@hotmail.com>
2022-02-11 16:19:27 +0100

    secured dashboard client-side

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useSession } from 'next-auth/react'
 
 const dashboard = () => {
+  const { data: session, status } = useSession()
+  const authenticated = status === 'authenticated'
+  const loading = status === "loading"
+
+  if(loading){
+    return <h2>Checking authentification</h2>
+  }
+
+  if(!authenticated){
+    return <h2>You need to be signed in to view this page</h2>
+  }
+
   return (
     <div>dashboard</div>
   )
