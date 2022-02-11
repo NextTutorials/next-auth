b45e48e495695f203d43611cd1d2a635f169ba9c
Johan Melin <soldierheart@hotmail.com>
2022-02-11 15:10:45 +0100

    updated authsyntax, added nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { SessionProvider } from "next-auth/react"
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import '../components/Navbar.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  )
 }
 
 export default MyApp
